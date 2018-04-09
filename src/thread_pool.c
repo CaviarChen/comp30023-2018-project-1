1eb916dedbaa03c4bc0b57b5012ad16df5a57ea7
Zijun Chen <z.chen70@student.unimelb.edu.au>
2018-04-09 21:24:21 +1000

    working on thread_pool

---
+++ src/thread_pool.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef void (*fun_ptr_t)(int worker_id, int arg);
+
+typedef struct task_s {
+    fun_ptr_t fun;
+    int arg;
+    struct task_s *next;
+} task_t;
+
+typedef struct thread_pool_s {
+    int thread_num;
+    int task_num;
+    struct task_s *head;
+    struct task_s *tail;
+
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+
+} thread_pool_t;
+
+
+void* thread_pool_init(int thread_num) {
+    thread_pool_t *tp = malloc(sizeof(thread_pool_t));
+    if(tp == NULL) return NULL;
+
+    tp->thread_num = thread_num;
+    tp->task_num = 0;
+    tp->head = NULL;
+    tp->tail = NULL;
+
+    pthread_mutex_init(&(tp->mutex), NULL);
+    pthread_cond_init(&(tp->cond), NULL);
+
+    pthread_t tid;
+    // create threads
+    for(int i=0; i<thread_num; i++) {
+        int *arg = malloc(sizeof(*arg));
+        *arg = i;
+        pthread_create(&tid, NULL, thread_run, arg);
+
+    }
+}
+
+void* thread_run(void *param) {
+    int worker_id = *((int *) i);
+    free(param);
+
+    return NULL
+}
